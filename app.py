e3529d1f16841947c0a0b9e495e98cb80599aefa
Вячеслав <s.urzhumov@outlook.com>
2023-11-10 12:24:12 +0500

    Исправление

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
 import OPi.GPIO as GPIO
+from flask import Flask, render_template
 from time import sleep
-from flask import Flask,render_template
+
 app = Flask(__name__)
-PIN=7 #(26 pin)
+PIN = 7 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(PIN,GPIO.OUT)
-GPIO.output(PIN,GPIO.LOW)
+GPIO.setup(PIN, GPIO.OUT)
+GPIO.output(PIN, GPIO.LOW)
 
 wsgi_app = app.wsgi_app
 
+
 @app.route("/")
 def home():
-    ledStat=GPIO.input(PIN)
-    data={
-        'led' : ledStat
-        }
-    return render_template('index.html', **data)
+    ledStat = GPIO.input(PIN)
+    data = {"led": ledStat}
+    return render_template("index.html", **data)
+
+
 @app.route("/<action>")
 def action(action):
     if action == "on":
-        GPIO.output(PIN,GPIO.HIGH)
+        GPIO.output(PIN, GPIO.HIGH)
     if action == "off":
-        GPIO.output(PIN,GPIO.LOW)
-    ledStat=GPIO.input(PIN)
-    data={
-        'led' : ledStat
-        }
-    return render_template('index.html',**data)
+        GPIO.output(PIN, GPIO.LOW)
+    ledStat = GPIO.input(PIN)
+    data = {"led": ledStat}
+    return render_template("index.html", **data)
 
-app.run (host="0.0.0.0", port=8500)
+
+app.run(host="0.0.0.0", port=8500)
